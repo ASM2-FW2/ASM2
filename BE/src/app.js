d75cafa1ceb779d55f43698c2663754da7a4e77e
phuongpew@gmail.com <phuongpew@gmail.com>
2023-08-01 19:33:25 +0700

    init

---
+++ BE/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", function () {
+  console.log(1);
+});
+mongoose.connect("mongodb://127.0.0.1:27017/ASM_FW2");
+
+export const viteNodeApp = app;
