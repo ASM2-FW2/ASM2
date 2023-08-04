c8565e9f946309a55458497b8069997f773ecb3a
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-08-04 10:25:28 +0700

    style:create ui productpage of the client

---
+++ Fe/src/Component/productPage/productPage.tsx
@@ -0,0 +1,86 @@
+
+const ProductPage = () => {
+    return (
+        <div>
+            <h2 className=''>Điện thoại nổi bật</h2>
+            <div className='grid grid-cols-6 gap-6 mb-3'>
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+
+                <div className='text-center m-auto ' >
+                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+
+                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
+                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductPage
