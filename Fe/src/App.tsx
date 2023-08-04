afd819b9c434186f537dec76833a976418325579
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-08-04 09:26:35 +0700

    style:create ui homePage

--- Fe/src/App.tsx
+++ Fe/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import HomePage from './page/homePage'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LayOutClt from './layout/layoutClient'
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<LayOutClt />}>
+          <Route index element={<HomePage />} />
+
+        </Route>
+      </Routes>
     </>
   )
 }
