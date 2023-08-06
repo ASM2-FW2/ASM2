042279b6728f6046461586d3803432206c8ff4a0
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-08-06 21:18:16 +0700

    feat:list product homePage

--- Fe/src/Component/productPage/productPage.tsx
+++ Fe/src/Component/productPage/productPage.tsx
@@ -1,89 +1,39 @@
+import { getAllProduct } from "../../action/Product";
+import { useAppDispatch, useAppSelector } from "../../store/hooks"
 import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
+import { useEffect } from 'react';
 const ProductPage = () => {
+    const dispatch = useAppDispatch()
+    const { products } = useAppSelector((state) => state.products)
+
+
+    useEffect(() => {
+        dispatch(getAllProduct())
+    }, [])
+    const productData = products?.data?.data || [];
+
     return (
         <div>
-            <Header/>
+            <Header />
             <div>
-            <h2 className=''>Điện thoại nổi bật</h2>
-            <div className='grid grid-cols-6 gap-6 mb-3'>
+                <h2 className=''>Điện thoại nổi bật</h2>
+                <div className='grid grid-cols-6 gap-6 mb-3'>
+                    {productData?.map((item: any, index: any) => (
+                        <div className='text-center m-auto ' key={index}>
+                            <img src={item.price} alt="" className='w-47 h-52' />
 
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
+                            <h3 className='font-semibold text-2xl py-2'>{item.name}</h3>
+                            <strong className="text-red-500">{item.price} <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
+                            <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+                        </div>
+                    ))}
 
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
+
+
                 </div>
-
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
-
-                <div className='text-center m-auto ' >
-                    <img src="https://upload.wikimedia.org/wikipedia/vi/b/b7/Doraemon1.jpg" alt="" className='w-47 h-52' />
-
-                    <h3 className='font-semibold text-2xl py-2'>sản phẩm 1</h3>
-                    <strong className="text-red-500">300 <sup>đ</sup> <del className='text-gray-400'><em>300 000</em>  <sup>đ</sup></del></strong> <br />
-                    <button className=' rounded-md p-1 bg-yellow-300 hover:bg-blue-600 hover:text-white'>mua ngay</button>
-                </div>
-
             </div>
-        </div>
-        <Footer/>
+            <Footer />
         </div>
     )
 }
